50238c2c8c4600cf358aac140cb81e38603ef7c7
Naveen7222 <96689459+Naveen7222@users.noreply.github.com>
2024-01-05 13:00:20 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -3,7 +3,20 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
+    const stack = [];
+    const brackets = { '(': ')', '{': '}', '[': ']' };
+
+    for (let char of s) {
+        if (char in brackets) {
+            stack.push(char);
+        } else {
+            const top = stack.pop();
+            if (brackets[top] !== char) {
+                return false; 
+            }
+        }
+    }
+    return stack.length === 0;
 };
 
 module.exports = { isValid };
